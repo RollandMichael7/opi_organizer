31806465eac52f271a55d15450fabf57894a47f9
Michael Rolland <rollandmichael7@gmail.com>
2018-06-26 14:36:34 -0400

    Handle invalid input and take command line arg

--- deploy/addPlugin.sh
+++ deploy/addPlugin.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 DETECTOR=/epics/support4c/areaDetector
-TARGET=./test/areaDetector
+TARGET=
 
-echo name of AD plugin to add:
-read plugin
-if [ -d $DETECTOR/$plugin ]; then
-	app="$(ls $DETECTOR/$plugin | grep App)"
-	mkdir -p $TARGET/$plugin
-	mkdir -p $TARGET/$plugin/$app
+if [ -z $TARGET ]; then
+	TARGET=$1
+fi
 
-	cp -r -n $DETECTOR/$plugin/bin $TARGET/$plugin
-	cp -r -n $DETECTOR/$plugin/db $TARGET/$plugin
-	cp -r -n $DETECTOR/$plugin/documentation $TARGET/$plugin
-	cp -r -n $DETECTOR/$plugin/iocs $TARGET/$plugin
-	cp -r -n $DETECTOR/$plugin/$app/Db $TARGET/$plugin/$app
-	cp -r -n $DETECTOR/$plugin/$app/op $TARGET/$plugin/$app
+if ! [ -z $TARGET ]; then
+	echo name of AD plugin to add:
+	read plugin
+	if [ -d $DETECTOR/$plugin ]; then
+		app="$(ls $DETECTOR/$plugin | grep App)"
+		mkdir -p $TARGET/areaDetector/$plugin
+		mkdir -p $TARGET/areaDetector/$plugin/$app
+	
+		cp -r -n $DETECTOR/$plugin/bin $TARGET/areaDetector/$plugin
+		cp -r -n $DETECTOR/$plugin/db $TARGET/areaDetector/$plugin
+		cp -r -n $DETECTOR/$plugin/documentation $TARGET/areaDetector/$plugin
+		cp -r -n $DETECTOR/$plugin/iocs $TARGET/areaDetector/$plugin
+		cp -r -n $DETECTOR/$plugin/$app/Db $TARGET/areaDetector/$plugin/$app
+		cp -r -n $DETECTOR/$plugin/$app/op $TARGET/areaDetector/$plugin/$app
+	else
+		echo Invalid name
+	fi
 else
-	echo Invalid name
+	echo Invalid TARGET. Did you set one?
 fi
