d54bc3ab93141f1ec19557c330e1ed8bb5674013
Michael Rolland <rollandmichael7@gmail.com>
2018-06-26 09:05:38 -0400

    Fix run script

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo Running convert_and_organize_ad.py
-python3 convert_and_organize_ad.py
+python3 ./convert_and_organize_ad.py
 echo Running convert_and_organize_epics.py
-python3 convert_and_organize_epics.py
-echo Running update_reference.py
-python3 update_references.py
+python3 ./convert_and_organize_epics.py
+echo Running update_references.py
+python3 update_references
+
