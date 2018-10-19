cae1aef2ce67b05a96ed4d6f2d63f1a05abbe8bd
Michael Rolland <rollandmichael7@gmail.com>
2018-10-19 10:51:52 -0400

    Use absolute paths

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+HOME=$(pwd)
 echo Running convert_and_organize_ad.py
-python3 ./organize_components/convert_and_organize_ad.py $@
+python3 $HOME/organize_components/convert_and_organize_ad.py $@
 echo Running convert_and_organize_epics.py
-python3 ./organize_components/convert_and_organize_epics.py $@
+python3 $HOME/organize_components/convert_and_organize_epics.py $@
 echo Running update_references.py
-python3 ./organize_components/update_references.py $@
+python3 $HOME/organize_components/update_references.py $@
